3c30c4f6d97461b2361729032a8ac5b404840b4d
henrykamoga <henryatubuntu@gmail.com>
2018-09-19 18:30:52 +0300

    To do list Application

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+// Register page
+// ======================
+var typed = new Typed(".type-wrap", {
+    stringsElement: '#type_my_strings',
+    typeSpeed: 100,
+    backSpeed: 0,
+    loop:true,
+    //startDelay:1000,
+
+    //shuffle: false,
+    fadeOut: false,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+    
+  });
